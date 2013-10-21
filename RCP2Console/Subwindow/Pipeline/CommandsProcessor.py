fe89c6ec03941d8fa76cff6aacd657fb7edc5d1d
Dmytro Konobrytskyi <aksonnn@gmail.com>
2013-10-21 16:43:56 -0400

    Support of the updated variable printing

--- RCP2Console/Subwindow/Pipeline/CommandsProcessor.py
+++ RCP2Console/Subwindow/Pipeline/CommandsProcessor.py
@@ -20,12 +20,11 @@ class CommandsProcessor(object):
         if "Commands" not in message.Info: return #No commands, no processing:) 
         
         commands = message.Info["Commands"].split(";")
-        map(string.strip, commands)
+        commands = map(string.strip, commands)
         
         for command in commands:
             commandName = command[0:command.find("(")]
             if commandName == "ParseJson":
                 message.Data.update(json.loads(message.Data["Value"]))
-            if commandName == "VariableName":
-                variableName = command[command.find("(")+1:-1]
-                message.Data["Value"] = "<b>%s</b> = %s"%(variableName, message.Data["Value"])
+            if commandName == "Variable":
+                message.Data["Value"] = "<b>%s</b> = %s"%(message.Data["Name"], message.Data["Value"])
